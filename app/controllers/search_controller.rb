b4209113d14f6d32f8b07c2f50c73ef752fa7855
Gil Gomes <gilgomesp@gmail.com>
2016-04-24 07:50:41 -0300

    saving searched term

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,17 @@
 class SearchController < ApplicationController
   def index
   	@search = params[:search]
+  	save_search if params[:search].present?
+  	
   	respond_to do |format|
   		format.html
   		format.js
   	end
+  end
+
+  private
+
+  def save_search
+  	Analyzer.create(query: params[:search], user_ip: request.ip)
   end
 end
