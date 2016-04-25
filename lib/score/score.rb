e14c952079ba5bd12ac54643f706c9508a1bb6a7
Gil Gomes <gilgomesp@gmail.com>
2016-04-25 13:04:17 -0300

    Documentation

--- lib/score/score.rb
+++ lib/score/score.rb
@@ -30,21 +30,55 @@ class Score
 
 	private
 
+	# Private: Normalize and Bring just the meaningful words based on
+	# the yaml list
+	#
+	# Examples
+	#
+	#   meaningful_words 'How do I cancel my account', ' '
+	#   # => ['cancel', 'account']
+	#
+	# Returns the array of meaningful words.
 	def meaningful_words phrase, divider=" "
 		phrase = normalize_phrase(phrase).split(divider)
 		filter(phrase)
 	end
 
-	def normalize_phrase(phrase)
+	# Private: Converts do string and downcase the phrase.
+	#
+	# Examples
+	#
+	#   normalize_phrase 'HOW DO I CANCEL MY ACCOUNT'
+	#   # => how do i cancel my account
+	#
+	# Returns the phrase after downcase it.
+	def normalize_phrase phrase
 		phrase.to_s.downcase
 	end
 
+	# Private: Bring just the meaningful words based on
+	# the yaml list
+	#
+	# Examples
+	#
+	#   meaningful_words 'How do I cancel my account', ' '
+	#   # => ['cancel', 'account']
+	#
+	# Returns the array of meaningful words.
 	def filter phrase
 		words = phrase.reject{ |word| stopwords_list.include? word }
-		words.reject{ |word| valid_word? word}
+		words.reject{ |word| invalid_word? word}
 	end
 
-	def valid_word? word
-		stopwords_list.collect{ |w| w if w.include? word }.uniq.any?
+	# Private: Checks if it is a middle word
+	#
+	# Examples
+	#
+	#   invalid_word? 'Ho'
+	#   # => true
+	#
+	# Returns the score.
+	def invalid_word? word
+		stopwords_list.collect{ |w| w if w.include? word.to_s.strip }.uniq.any?
 	end
 end
