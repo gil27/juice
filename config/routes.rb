f34b3ec323436acb270bdeb7ee94f899d0b036de
Gil Gomes <gilgomesp@gmail.com>
2016-04-25 21:55:47 -0300

    Stats controller, putting some ideas before add graphs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
+
   root to: 'search#index'
   mount Sidekiq::Web => '/sidekiq'
   
   get 'search/index', as: :search
+
+  get 'stats' => 'stats#index'
 end
